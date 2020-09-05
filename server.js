e44ba30dd379bda96d2dfc98f97df735a9c0d1f7
njohnerwin <njohnerwin@gmail.com>
2020-09-05 17:33:50 -0400

    Implemented OAUTH authentication - WoW API calls should work properly now

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exphbs = require("express-handlebars");
 const BnetStrategy = require('passport-bnet').Strategy;
 const BNET_ID = process.env.BNET_ID
 const BNET_SECRET = process.env.BNET_SECRET
+require("dotenv").config();
  
 
 // Requiring passport as we've configured it
@@ -30,6 +31,8 @@ app.use(passport.session());
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+
+require("./interval.js");
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
