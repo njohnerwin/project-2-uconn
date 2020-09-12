de25fee91c67b5bb46d37f68b2851d603adb5207
njohnerwin <njohnerwin@gmail.com>
2020-09-12 01:13:20 -0400

    Fixed API data appends, but style is still broken for some reason.

--- public/js/teaminfo.js
+++ public/js/teaminfo.js
@@ -76,19 +76,23 @@ $(document).ready(function () {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-    }
-    printWoWProfile(realmslug, (this.id).toLowerCase(), content);
+      if ($(`#${this.id}-sys`).attr("data-success") == "true") {
+        return;
+      } printWoWProfile(realmslug, this.id);
+    }  
   });
 
   function printMemberCard(member) {
-    let memberCard = $(`<div class="member-card" id="${member.name}">${member.name} || ${member.clss}</div>`);
-    let cardContent = $(`<div class="card-content" id="${member.name}-content"></div>`)
+    let nameID = (member.name).toLowerCase();
+    let memberCard = $(`<div class="member-card" id="${nameID}">${member.name} || ${member.clss}</div>`);
+    let cardContent = $(`<div class="card-content" id="${nameID}-content"></div>`)
 
-    cardContent.append($(`<p id="race-field">Race: </p>`));
-    cardContent.append($(`<p id="gender-field">Gender: </p>`));
-    cardContent.append($(`<p id="spec-field">Spec: </p>`));
-    cardContent.append($(`<p id="clvl-field">Level: </p>`));
-    cardContent.append($(`<p id="ilvl-field">ILVL: </p>`));
+    cardContent.append($(`<p class="datapoint" id=${nameID}-race>Race: </p>`));
+    cardContent.append($(`<p class="datapoint" id="${nameID}-gender">Gender: </p>`));
+    cardContent.append($(`<p class="datapoint" id="${nameID}-spec">Spec: </p>`));
+    cardContent.append($(`<p class="datapoint" id="${nameID}-clvl">Level: </p>`));
+    cardContent.append($(`<p class="datapoint" id="${nameID}-ilvl">ILVL: </p>`));
+    cardContent.append($(`<p class="sys-message" id="${nameID}-sys">No data for ${member.name}</p>`))
 
     if (member.id > 0) {
       switch (member.role) {
@@ -147,7 +151,7 @@ $(document).ready(function () {
     });
   }
 
-  function printWoWProfile(realm, charname, dataField) {
+  function printWoWProfile(realm, charname) {
     $.get("/api/wow").then(function (data) {
 
       //We use the "/api/wow" route to grab the api key from the local files via Node and dotenv - see in api-routes.js for more detail
@@ -158,14 +162,20 @@ $(document).ready(function () {
 
       $.ajax({
         url: queryURL,
-        method: "GET"
-      }).then(function (response) {
+        method: "GET",
+        error: function() {
+          console.log("Error looking up that stuff");
+          $(`#${charname}-content`).addClass("card-content-dead");
+        }
+      }).then(function(response) {
         console.log(response);
-        dataField.children[0].append(response.race.name);
-        dataField.children[1].append(response.gender.name);
-        dataField.children[2].append(response.active_spec.name);
-        dataField.children[3].append(response.level);
-        dataField.children[4].append(response.average_item_level);
+        $(`#${charname}-race`).append(response.race.name);
+        $(`#${charname}-gender`).append(response.gender.name);
+        $(`#${charname}-spec`).append(response.active_spec.name);
+        $(`#${charname}-clvl`).append(response.level);
+        $(`#${charname}-ilvl`).append(response.average_item_level);
+        $(`#${charname}-sys`).text(`Data for ${response.name}`);
+        $(`#${charname}-sys`).attr("data-success", "true");
       });
     });
   }
